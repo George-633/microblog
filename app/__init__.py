4df37d8099a35c8dc0dc86d08ff887f2f1fb46bc
George-633 <112063126@qq.com>
2019-12-19 19:15:28 +0800

    SQL add part

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,11 @@ import sys
 sys.path.append('..')
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
 
-from app import routes
+from app import routes#, models
--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 class Config(object):
     SQLALCHEMY_DATABASE_URI = 'mysql://root:123456@127.0.0.1/flaskblog?charset=utf8'
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'lssgeorge633fromdlbj'
