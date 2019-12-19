a149e89ae8882be1646bad17312d8a08e653646b
George-633 <112063126@qq.com>
2019-12-19 18:55:09 +0800

    add template step

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,29 @@
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts, comments=['first', 'second'])
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('用户：{}，恭喜你成功注册'.format(form.username.data))
+        return redirect(url_for('index'))
+    return render_template('login.html',  title='Sign In', form=form)
---
+++ config.py
@@ -0,0 +1,4 @@
+class Config(object):
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:123456@127.0.0.1/flaskblog?charset=utf8'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'lssgeorge633fromdlbj'
